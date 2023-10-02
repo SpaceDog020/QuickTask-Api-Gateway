bf927d2b24d2a75bcb6dc1a99929a92e7f536523
Nicolas <nicolas.bravo@alumnos.ucn.cl>
2023-10-02 15:16:59 -0300

    feat: add connection with graphql

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
+      driver: ApolloGatewayDriver,
+      gateway: {
+        supergraphSdl: new IntrospectAndCompose({
+          subgraphs: [
+            {
+              name: 'users',
+              url: 'http://localhost:3001/graphql',
+            },
+            {
+              name: 'tasks',
+              url: 'http://localhost:3002/graphql',
+            },
+          ],
+        }),
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
